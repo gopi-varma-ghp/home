c9632b35768b192db784e57494c47bbc22a85c12
gopi-varma-ghp <dongarigopi3@gmail.com>
2025-05-14 14:37:29 +0530

    Add files via upload

---
+++ Loop2.java
@@ -0,0 +1,13 @@
+
+public class Loop {
+	public static void main(String[]args) {
+		for(int i=10;i>=1; i=i-1 ) {
+			System.out.println(i);
+		
+		
+		
+	}
+}
+
+	
+}	
---
+++ Loop5.java
@@ -0,0 +1,11 @@
+public class Loop {
+	public static void main(String[]args) {
+		for(int i=5; i<=50; i=i+5) {
+		System.out.println(i);
+		
+		
+	}
+}
+
+	
+}	
